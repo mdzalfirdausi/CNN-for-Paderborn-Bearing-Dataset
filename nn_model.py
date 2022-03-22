4d19560c5ca08fb64b96c243531b47ab3ef2105a
Muhammad Firdausi <79037461+mdzalfirdausi@users.noreply.github.com>
2022-03-22 10:19:32 +0300

    Update nn_model.py

--- nn_model.py
+++ nn_model.py
@@ -1,77 +1,27 @@
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=4,stride=1,padding = 1)
+        # self.conv1 = nn.Conv2d(2, 32, kernel_size=4,stride=1,padding = 1)
+        # self.conv1 = nn.Conv2d(1, 32, kernel_size=4,stride=1,padding = 1)
+        self.mp1 = nn.MaxPool2d(kernel_size=3,stride=3)
+        self.conv2 = nn.Conv2d(64,128, kernel_size=3,stride =1)
+        self.mp2 = nn.MaxPool2d(kernel_size=5,stride=3)
+        # self.fc1= nn.Linear(2304,256)
+        # self.fc1= nn.Linear(5184,256)
+        self.fc1= nn.Linear(2048,256)
+        self.dp1 = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(256,3)
 
-import torch
-from torch import nn
-from torch.nn import functional as F
-# 2 Layers CNN
-class CNN_1D_2L(nn.Module):
-    def __init__(self, n_in):
-        super().__init__()
-        self.n_in = n_in
-        self.layer1 = nn.Sequential(
-            nn.Conv1d(1, 64, (9,), stride=1, padding=4),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.MaxPool1d(2,stride=2)
-        )
-        
-        self.layer2 = nn.Sequential(
-            nn.Conv1d(64, 128, (5,), stride=1, padding=2),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.AvgPool1d(2,stride=2)
-        )
-        
-        self.linear1 = nn.Linear(self.n_in*128 //4, 4)
-
-        
     def forward(self, x):
-        x = x.view(-1, 1, self.n_in)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = x.view(-1, self.n_in*128//4)
-        return self.linear1(x)
-
-
-# 3 Layers CNN
-class CNN_1D_3L(nn.Module):
-    def __init__(self, n_in):
-        super().__init__()
-        self.n_in = n_in
-        self.layer1 = nn.Sequential(
-            nn.Conv1d(1, 64, (9,), stride=1, padding=4),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.MaxPool1d(2,stride=2)
-        )
-                
-        self.layer2 = nn.Sequential(
-            nn.Conv1d(64, 64, (5,), stride=1, padding=2),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.MaxPool1d(2,stride=2)
-        )
+        in_size = x.size(0)
+        # x = F.relu(self.mp1(self.conv1(x)))
+        # x = F.relu(self.mp2(self.conv2(x)))
+        x = self.mp1(F.relu(self.conv1(x)))
+        x = self.mp2(F.relu(self.conv2(x)))
+        x = x.view(in_size,-1)
+        x = F.relu(self.fc1(x))
+        x = self.dp1(x)
+        x = self.fc2(x)
         
-        self.layer3 = nn.Sequential(
-            nn.Conv1d(64, 128, (5,), stride=1, padding=2),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.MaxPool1d(2,stride=2)
-        )
-        
-        self.linear1 = nn.Linear(self.n_in*128 //8, 4)
-
-        
-    def forward(self, x):
-        x = x.view(-1, 1, self.n_in)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        #x = x.view(-1, self.n_in*128//4)
-        x = x.view(-1, self.n_in*128//8)
-        return self.linear1(x)
-        
+        return F.log_softmax(x, dim=1)
